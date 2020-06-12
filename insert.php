243a5003bf0d64341a863942d2849d4ae4916686
Konduru Venkata Chandu <58496589+venkatachandu08@users.noreply.github.com>
2020-06-12 22:16:32 +0530

    Stores data in database
    This stores data in database and posts the user's data in the consecutive page

---
+++ insert.php
@@ -0,0 +1,57 @@
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+$gender = $_POST['gender'];
+$email = $_POST['email'];
+$phoneCode = $_POST['phoneCode'];
+$phone = $_POST['phone'];
+
+echo "Username: " . $username;
+echo nl2br("\n");
+echo "Password: ". $password;
+echo nl2br("\n");
+echo "Gender: ". $gender;
+echo nl2br("\n");
+echo "Email: ". $email;
+echo nl2br("\n");
+echo "Phone Number: " . $phone;
+echo nl2br("\n");
+echo nl2br("\n");
+echo nl2br("\n");
+echo nl2br("\n");
+if (!empty($username) || !empty($password) || !empty($gender) || !empty($email) || !empty($phoneCode) || !empty($phone)) {
+ $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbname = "youtube";
+    //create connection
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+    if (mysqli_connect_error()) {
+     die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+    } else {
+     $SELECT = "SELECT email From register Where email = ? Limit 1";
+     $INSERT = "INSERT Into register (username, password, gender, email, phoneCode, phone) values(?, ?, ?, ?, ?, ?)";
+     //Prepare statement
+     $stmt = $conn->prepare($SELECT);
+     $stmt->bind_param("s", $email);
+     $stmt->execute();
+     $stmt->bind_result($email);
+     $stmt->store_result();
+     $rnum = $stmt->num_rows;
+     if ($rnum==0) {
+      $stmt->close();
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("ssssii", $username, $password, $gender, $email, $phoneCode, $phone);
+      $stmt->execute();
+      echo "<h2>"."New record inserted sucessfully" ."<h2>";
+     } else {
+      echo "Someone already register using this email";
+     }
+     $stmt->close();
+     $conn->close();
+    }
+} else {
+ echo "All field are required";
+ die();
+}
+?>
